783889a9ebf895f57b547bde0f050d5d6ea25c2e
Emmanuel Palafox <emmanuelpalafox@gmail.com>
2020-06-29 09:49:26 -0500

    Init project

---
+++ ExtractFilesFromFolder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExtractFilesFromFolder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("Usage:");
+				Console.WriteLine("exe \"[path]\"");
+			}
+			else
+			{
+				foreach (var path in args)
+				{
+					if (System.IO.Directory.Exists(path))
+					{
+						var children = System.IO.Directory.GetDirectories(path);
+						foreach(var child in children)
+							RecursiveFolder(child, path);
+					}
+				}
+			}
+		}
+		static void RecursiveFolder(string path, string parent)
+		{
+			var list = System.IO.Directory.GetDirectories(path);
+			foreach (var file in list)
+			{
+				RecursiveFolder(file, parent);
+			}
+			foreach(var file in System.IO.Directory.GetFiles(path))
+			{
+				var fileName = System.IO.Path.GetFileName(file);
+				var newFile = System.IO.Path.Combine(parent, fileName);
+				var suffix = 0;
+				while(System.IO.File.Exists(newFile))
+				{
+					newFile = $"{newFile.Split('.')[0]}_{suffix}.{newFile.Split('.')[1]}";
+				}
+				Console.WriteLine($"Moving: {file} to {newFile}");
+				System.IO.File.Copy(file, newFile);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Extract Files from
+
+Extract all files inside folders of the "args" directories to their root.
+
+## Debug
+
+Modify in Properties/launchSettings.json
+
+```json
+{
+  "profiles": {
+    "ExtractFilesFromFolder": {
+      "commandName": "Project",
+      "commandLineArgs": "\"G:\\fotos mac\""
+    }
+  }
+}
+```
+Change commandLineArgs to your desired path.
